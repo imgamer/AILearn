b15fc9bf6d3b9c2d9ca4adfc22737dde31ec1e80
shufeng <shufengw@qq.com>
2024-10-15 06:53:08 +0800

    debug_practice代码

---
+++ AITrainer/10.13/debug_practice/debug_practice.py
@@ -0,0 +1,26 @@
+import cv2
+
+lena = cv2.imread("image/lena03.png", 1)
+logo = cv2.imread("image/anaconda.png", 1)
+
+logo = cv2.resize(logo,(400,300))
+
+h1, w1, ch1 = lena.shape
+h2, w2, ch2 = logo.shape
+roi = lena[h1-h2:h1, w1-w2:w1]
+
+grey = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
+
+ret, mask1 = cv2.threshold(grey, 220, 255, cv2.THRESH_BINARY)
+
+fg1 = cv2.bitwise_and(roi, roi, mask=mask1)
+
+ret, mask2 = cv2.threshold(grey, 220, 255, cv2.THRESH_BINARY_INV)
+
+fg2 = cv2.bitwise_and(logo, logo, mask=mask2)
+
+roi[:] = cv2.add(fg1,fg2)
+cv2.imshow('roi2', roi)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
