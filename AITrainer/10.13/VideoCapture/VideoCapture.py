e130734d72fb54249018fc4b617bf3a0d76027e5
shufeng <imgamer@163.com>
2024-10-30 08:53:54 +0800

    1

--- AITrainer/10.13/VideoCapture/VideoCapture.py
+++ AITrainer/10.13/VideoCapture/VideoCapture.py
@@ -1,3 +1,27 @@
+"""
+使用当前设备摄像头采集10张图像
+按下s键，保存当前帧到指定目录
+按下q键，退出程序
+
+需求分析：
+把图像保存到哪里？  建立目录或者保存到当前工作目录。
+如何采集当前帧？      cap.read()
+如何保存当前帧？      cv2.imwrite()
+如何接受和处理键盘事件？ cv2.waitkey()
+程序是持续运行直到按下q键才会退出。 while true
+程序运行到采集了10张图像也可以主动退出。while count == 10
+持续运行的过程中，接受键盘事件，判断是否按下了s键，按下了就保存当前帧，并继续循环。
+
+本程序使用OpenCV库，需要先安装OpenCV库才能运行
+
+1. 是要理解视频数据是由离散的图像帧组成的，显示视频就是获取每一帧然后用vs2显示。camrea.read()
+2. 检测键盘事件和处理
+3. 熟练使用cv2.imshow()和cv2.imwrite()函数
+
+主要逻辑是抓取图像并保存，但这之前要先准备好环境，建立目录，初始化摄像头
+
+"""
+
 import cv2
 import os
 
@@ -6,9 +30,8 @@ output_folder = 'captured_images'
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-# 初始化摄像头
+# 初始化摄像头，摄像头开始拍摄，0表示第一个摄像头
 cap = cv2.VideoCapture(0)
-
 # 检查摄像头是否成功打开
 if not cap.isOpened():
     print("无法打开摄像头")
